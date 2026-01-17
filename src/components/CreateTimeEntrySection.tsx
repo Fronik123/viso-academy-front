dee6b8223c61d9d379ccfa012ee7a050273f86c0
fronik <askolya23@gmail.com>
2026-01-17 22:05:07 +0200

    [*] - refactor code

--- src/components/CreateTimeEntrySection.tsx
+++ src/components/CreateTimeEntrySection.tsx
@@ -2,19 +2,14 @@
 
 import TimeEntryForm from "./TimeEntryForm";
 import { useCreateTimeEntry } from "@/hooks/useCreateTimeEntry";
-import { CreateTimeEntry } from "@/types/timeEntry";
 
 export default function CreateTimeEntrySection() {
   const { submit, isSubmitting, error, success, clearError, clearSuccess } = useCreateTimeEntry();
 
-  const handleSubmit = async (data: CreateTimeEntry): Promise<boolean> => {
-    return await submit(data);
-  };
-
   return (
     <div className="mb-12 flex justify-center">
       <TimeEntryForm
-        onSubmit={handleSubmit}
+        onSubmit={submit}
         isSubmitting={isSubmitting}
         error={error}
         success={success}
